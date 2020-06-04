6e15c38cf1a63e9dea4239f269ff38001443af10
Michał Kopeć <mkopec12@gmail.com>
2020-06-04 19:28:59 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Close Tabs to the Right
+This WebExtension (re)adds a context menu option to browser tabs to close tabs to the right of the selected one.
+Tested on Firefox 55.
---
+++ background.js
@@ -0,0 +1,58 @@
+function onCreated(n) {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+const menuItemParams = {
+  id: "close_right",
+  title: browser.i18n.getMessage("contextItemTitle"),
+  contexts: ["tab"]
+};
+browser.contextMenus.create(menuItemParams, onCreated);
+
+function closeTabs(sender, tabs) {
+    var senderFound = false;
+    for (var tab of tabs) {
+        if (tab.id == sender.id) {
+            senderFound = true;
+            continue;
+        }
+        if (!tab.pinned && senderFound) {
+            browser.tabs.remove(tab.id);
+        }
+    }
+}
+
+browser.contextMenus.onClicked.addListener(function(info, sender) {
+    var querying = browser.tabs.query({currentWindow: true});
+
+    querying.then(closeTabs.bind(null, sender));
+});
+
+
+// supports Tree Style Tab's fake context menu
+// https://addons.mozilla.org/firefox/addon/tree-style-tab/
+function registerToTST() {
+    browser.runtime.sendMessage("treestyletab@piro.sakura.ne.jp", {
+        type: "fake-contextMenu-create",
+        params: menuItemParams
+    }).then(onCreated, onCreated);
+}
+browser.runtime.onMessageExternal.addListener((message, sender) => {
+    switch (sender.id) {
+        case "treestyletab@piro.sakura.ne.jp":
+            switch (message.type) {
+                case "ready":
+                    registerToTST();
+                    return;
+                case "fake-contextMenu-click":
+                    browser.tabs.query({currentWindow: true})
+                        .then(tabs => closeTabs(message.tab, tabs));
+                    return;
+            }
+    }
+});
+registerToTST();
